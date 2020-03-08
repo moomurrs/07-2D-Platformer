6dfc9e5d97a0d237bba273fef15190c9f42e14de
moomurrs <59849596+moomurrs@users.noreply.github.com>
2020-03-07 20:32:38 -0500

    working animation and added tilemaps
    player can move and change sprites on map

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -15,17 +15,25 @@ var on_ground = false
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("run")
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("run")
 	else:
 		# default: do not move x
 		velocity.x = 0
+		# play the idle animation only if player is not moving AND is on ground
+		if on_ground:
+			$AnimatedSprite.play("idle")
 	
 	if Input.is_action_pressed("ui_up"):
 		
 		# only jump when on ground
 		if on_ground == true:
 			velocity.y = JUMP_POWER
+			
 			on_ground = false
 	
 	# bring the player down due to gravity
@@ -35,6 +43,12 @@ func _physics_process(delta):
 		on_ground = true
 	else:
 		on_ground = false
+		# if player is moving upwards and isn't on ground, play jump animation
+		if velocity.y < 0:
+			$AnimatedSprite.play("jump")
+		else:
+			# if player is moving down and isn't on ground play fall animation
+			$AnimatedSprite.play("fall")
 	
 	# move player according to updated vector
 	velocity = move_and_slide(velocity, FLOOR)
